3d2d71be2ad04a9e0f12ab4c19bffb6ac66440d4
Lara Miñones Rodríguez <lminones@ficodes.com>
2025-12-15 10:54:32 +0100

    FIX search pagination cache

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -8,9 +8,10 @@ import { PaginationService } from 'src/app/services/pagination.service'
 import {LocalStorageService} from "../../services/local-storage.service";
 import {Category} from "../../models/interfaces";
 import {EventMessageService} from "../../services/event-message.service";
+import { SearchStateService } from "../../services/search-state.service"
 import { LoginServiceService } from "src/app/services/login-service.service"
 import { environment } from 'src/environments/environment';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationStart } from '@angular/router';
 import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginInfo, FeedbackInfo } from 'src/app/models/interfaces';
@@ -38,6 +39,7 @@ export class SearchComponent implements OnInit, OnDestroy {
   showPanel = false;
   feedback:boolean=false;
   providerThemeName=environment.providerThemeName;
+  private navigatingToDetail = false;
 
   constructor(
     private api: ApiServiceService,
@@ -47,9 +49,13 @@ export class SearchComponent implements OnInit, OnDestroy {
     private localStorage: LocalStorageService,
     private eventMessage: EventMessageService,
     private loginService: LoginServiceService,
-    private paginationService: PaginationService) {
-    this.eventMessage.messages$.subscribe(ev => {
+    private paginationService: PaginationService,
+    private state: SearchStateService
+    ) {
+    this.eventMessage.messages$.subscribe(async ev => {
       if(ev.type === 'AddedFilter' || ev.type === 'RemovedFilter') {
+        console.log('event filter')
+        await this.getProducts(false);
         this.checkPanel();
       }
     })
@@ -61,6 +67,17 @@ export class SearchComponent implements OnInit, OnDestroy {
   } 
 
   async ngOnInit() {
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        // Detecta navegación al detalle del producto
+        if (event.url.startsWith('/search/urn:ngsi-ld:product-offering')) {
+          this.navigatingToDetail = true;
+        } else {
+          this.navigatingToDetail = false;
+        }
+      }
+    });
     
     this.products=[];
     this.nextProducts=[];
@@ -73,13 +90,33 @@ export class SearchComponent implements OnInit, OnDestroy {
       this.searchField.setValue(this.keywords);
     }
     console.log('INIT')
+
+    // 1. Restaurar estado
+    if (this.state.hasState()) {
+      console.log("Restoring state…");
+
+      this.products = this.state.products;
+      this.nextProducts = this.state.nextProducts;
+      this.page = this.state.page;
+      this.page_check = this.state.page_check;
+      this.keywords = this.state.keywords;
+
+      // restaurar campo de búsqueda
+      this.searchField.setValue(this.keywords);
+
+      return; // <-- IMPORTANTE: evitar volver a cargar desde cero
+    }
+
+    // Si no hay estado, entonces sí iniciar búsqueda normal
     await this.getProducts(false);
-    await this.eventMessage.messages$.subscribe(async ev => {
+
+
+    /*await this.eventMessage.messages$.subscribe(async ev => {
       if(ev.type === 'AddedFilter' || ev.type === 'RemovedFilter') {
         console.log('event filter')
         await this.getProducts(false);
       }
-    })
+    })*/
 
     let input = document.querySelector('[type=search]')
     if(input!=undefined){
@@ -160,6 +197,10 @@ export class SearchComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(){
+    if (this.navigatingToDetail) {
+      return;
+    }
+
     let storedFilters = this.localStorage.getObject('selected_categories') as Category[] || [];
     for(let i=0;i<storedFilters.length;i++){
       this.localStorage.removeCategoryFilter(storedFilters[i]);
@@ -172,6 +213,9 @@ export class SearchComponent implements OnInit, OnDestroy {
     if(next==false){
       this.loading=true;
     }
+    if (!next) {
+      this.state.clear();
+    }    
     
     let options = {
       "keywords": this.keywords,
@@ -180,12 +224,23 @@ export class SearchComponent implements OnInit, OnDestroy {
 
     this.paginationService.getItemsPaginated(this.page, this.PRODUCT_LIMIT, next, this.products,this.nextProducts, options,
       this.paginationService.getProducts.bind(this.paginationService)).then(async data => {
-        this.products=await this.api.getProductsDetails(data.items);
-        this.page_check=data.page_check;
-        this.nextProducts=await this.api.getProductsDetails(data.nextItems);
-        this.page=data.page;
-        this.loading=false;
-        this.loading_more=false;
+        this.products = await this.api.getProductsDetails(data.items);
+        this.nextProducts = await this.api.getProductsDetails(data.nextItems);
+      
+        this.page = data.page;
+        this.page_check = data.page_check;
+      
+        this.loading = false;
+        this.loading_more = false;
+      
+        // SAVE STATE 🔥🔥🔥
+        this.state.save({
+          products: this.products,
+          nextProducts: this.nextProducts,
+          page: this.page,
+          page_check: this.page_check,
+          keywords: this.keywords
+        });
     })
 
   }
