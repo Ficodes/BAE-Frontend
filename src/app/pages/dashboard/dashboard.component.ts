67ce546af27c60f22d2a3f0a6c17c549dd0ea834
Francisco de la Vega <fdelavega@ficodes.com>
2026-02-12 11:57:13 +0100

    Update the landing page to recover theme support

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,11 @@
 import { SlicePipe } from '@angular/common';
-import { ChangeDetectorRef, Component, OnInit, SecurityContext } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit, SecurityContext } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { initFlowbite } from 'flowbite';
 import * as moment from 'moment';
-import { map, Subject, takeUntil } from 'rxjs';
+import { map, Subject, Subscription, takeUntil } from 'rxjs';
 import { LoginInfo } from 'src/app/models/interfaces';
 import { ProductOffering } from 'src/app/models/product.model';
 import { EventMessageService } from 'src/app/services/event-message.service';
@@ -13,6 +13,7 @@ import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { LoginServiceService } from 'src/app/services/login-service.service';
 import { ApiServiceService } from 'src/app/services/product-service.service';
 import { StatsServiceService } from 'src/app/services/stats-service.service';
+import { ThemeService } from 'src/app/services/theme.service';
 import { EuropeTrademarkComponent } from 'src/app/shared/europe-trademark/europe-trademark.component';
 import { ThemeConfig } from 'src/app/themes';
 import { environment } from 'src/environments/environment';
@@ -29,13 +30,23 @@ interface Stats {
   standalone: true,
   imports: [TranslateModule, SlicePipe, EuropeTrademarkComponent],
 })
-export class DashboardComponent implements OnInit {
-  private unSub = new Subject();
+export class DashboardComponent implements OnInit, OnDestroy {
+  private unSub = new Subject<void>();
+  private rotationIntervalId?: ReturnType<typeof setInterval>;
+  private themeSubscription: Subscription = new Subscription();
+
   productOfferings?: ProductOffering[];
   protected MAX_CATEGORIES_PER_PRODUCT_OFFERING = 3;
 
   providerThemeName = environment.providerThemeName;
   currentTheme: ThemeConfig | null = null;
+  domeRegister: string = environment.DOME_REGISTER_LINK;
+
+  services: string[] = [];
+  publishers: string[] = [];
+  currentIndexServ = 0;
+  currentIndexPub = 0;
+  delay = 2000;
 
   stats?: Stats;
 
@@ -48,90 +59,100 @@ export class DashboardComponent implements OnInit {
     private eventMessage: EventMessageService,
     private cdr: ChangeDetectorRef,
     private router: Router,
-    private statsService: StatsServiceService
-  ) { }
+    private statsService: StatsServiceService,
+    private themeService: ThemeService,
+  ) {}
 
   ngOnInit() {
+    this.themeSubscription = this.themeService.currentTheme$.subscribe((theme) => {
+      this.currentTheme = theme;
+    });
+
     this.getFirstThreeRandomProductOfferings();
     this.checkRouteForToken();
-    this.getStats()
+    this.getStats();
   }
 
+  private startTagTransition() {
+    if (this.rotationIntervalId) {
+      clearInterval(this.rotationIntervalId);
+    }
+
+    this.rotationIntervalId = setInterval(() => {
+      if (this.services.length > 0) {
+        this.currentIndexServ = (this.currentIndexServ + 1) % this.services.length;
+      }
+      if (this.publishers.length > 0) {
+        this.currentIndexPub = (this.currentIndexPub + 1) % this.publishers.length;
+      }
+    }, this.delay);
+  }
 
   private getStats() {
-    this.statsService.getStats().then(data => {
+    this.statsService.getStats().then((data) => {
+      this.services = data?.services || [];
+      this.publishers = data?.organizations || [];
+
       this.stats = {
-        services: data?.services?.length || 0,
-        providers: data?.organizations?.length || 0
-      }
-    })
+        services: this.services.length,
+        providers: this.publishers.length,
+      };
+
+      this.startTagTransition();
+    });
   }
 
   private checkRouteForToken() {
     if (this.route.snapshot.queryParamMap.get('token') != null) {
-      this.loginService.getLogin(this.route.snapshot.queryParamMap.get('token')).then(data => {
-        console.log('---- loginangular response ----')
-        console.log(data)
-        console.log(data.username)
-
+      this.loginService.getLogin(this.route.snapshot.queryParamMap.get('token')).then((data) => {
         const info = {
-          "id": data.id,
-          "user": data.username,
-          "email": data.email,
-          "token": data.accessToken,
-          "expire": data.expire,
-          "partyId": data.partyId,
-          "roles": data.roles,
-          "organizations": data.organizations,
-          "logged_as": data.id
+          id: data.id,
+          user: data.username,
+          email: data.email,
+          token: data.accessToken,
+          expire: data.expire,
+          partyId: data.partyId,
+          roles: data.roles,
+          organizations: data.organizations,
+          logged_as: data.id,
         } as LoginInfo;
 
         // Using organization session by default if provided
         if (info.organizations != null && info.organizations.length > 0) {
-          info.logged_as = info.organizations[0].id
+          info.logged_as = info.organizations[0].id;
         }
 
         this.localStorage.addLoginInfo(info);
         this.eventMessage.emitLogin(info);
         initFlowbite();
-        console.log('----')
-
-      })
-      this.router.navigate(['/dashboard'])
+      });
+      this.router.navigate(['/dashboard']);
     } else {
-      console.log('sin token')
       const aux = this.localStorage.getObject('login_items') as LoginInfo;
       if (JSON.stringify(aux) != '{}') {
-        console.log(aux)
-        console.log('moment')
-        console.log(aux['expire'])
-        console.log(moment().unix())
-        console.log(aux['expire'] - moment().unix())
-        console.log(aux['expire'] - moment().unix() <= 5)
+        console.log(aux);
+        console.log('moment');
+        console.log(aux['expire']);
+        console.log(moment().unix());
+        console.log(aux['expire'] - moment().unix());
+        console.log(aux['expire'] - moment().unix() <= 5);
       }
     }
 
-
     this.cdr.detectChanges();
-    console.log('----')
   }
 
   private getFirstThreeRandomProductOfferings(): void {
     this.productService
       .getAllProducts()
       .pipe(
-
         map((items) =>
           items.map((el) => ({
             ...el,
             description: el.description
-              ? (this.domSanitizer.sanitize(
-                SecurityContext.HTML,
-                el.description,
-              ) ?? undefined)
+              ? (this.domSanitizer.sanitize(SecurityContext.HTML, el.description) ?? undefined)
               : el.description,
           })),
-
         ),
         map((items) => {
           const result = new Set<number>();
@@ -143,7 +164,6 @@ export class DashboardComponent implements OnInit {
 
           return [...result].map((i) => items[i]);
         }),
-
         takeUntil(this.unSub),
       )
       .subscribe((picked) => {
@@ -155,9 +175,21 @@ export class DashboardComponent implements OnInit {
     this.router.navigate(['/search']);
   }
 
-  ngOnDestroy() {
-    this.unSub.complete();
-    this.unSub.unsubscribe();
+  hasLongWord(str: string | undefined, threshold = 20) {
+    if (str) {
+      return str.split(/\s+/).some((word) => word.length > threshold);
+    }
+    return false;
   }
 
+  ngOnDestroy() {
+    if (this.rotationIntervalId) {
+      clearInterval(this.rotationIntervalId);
+    }
+    if (this.themeSubscription) {
+      this.themeSubscription.unsubscribe();
+    }
+    this.unSub.next();
+    this.unSub.complete();
+  }
 }
