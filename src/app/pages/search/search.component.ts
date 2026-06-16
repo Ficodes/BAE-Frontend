fc77e4d3446d0db5551a4f6ba13b575736c5769e
Francisco de la Vega <fdelavega@ficodes.com>
2026-06-16 16:01:29 +0200

    Uncomment sort

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -506,11 +506,10 @@ export class SearchComponent implements OnInit, OnDestroy {
       "keywords": this.keywords,
       "filters": filters
     }
-    // Ordering is disabled until the backend supports sorting search results.
-    // const sortParam = this.getSortParam();
-    // if (sortParam !== undefined) {
-    //   options.sort = sortParam;
-    // }
+    const sortParam = this.getSortParam();
+    if (sortParam !== undefined) {
+      options.sort = sortParam;
+    }
 
     try {
       const data = await this.paginationService.getItemsPaginated(
